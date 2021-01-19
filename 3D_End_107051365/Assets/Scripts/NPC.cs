912bfdc882fe1790f9ad7910be1c651f61a72a49
hy0817 <72055112+hy0817@users.noreply.github.com>
2021-01-19 15:47:08 +0800

    完成角色

---
+++ 3D_End_107051365/Assets/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "忍者")
+        {
+            playerInArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "忍者")
+        {
+            playerInArea = false;
+        }
+    }
+ 
+}
---
+++ 3D_End_107051365/Assets/Scripts/enemy.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    
+}
